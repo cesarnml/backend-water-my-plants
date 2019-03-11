e12ff558dbc4a2da9ce4ce565938447854c18e58
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-11 11:46:23 -0700

    adds sanity check endpoint

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const server = require('./server.js')
 const port = process.env.PORT || 4000
 
+//* "Sanity Check"
+server.get('/', (req, res) => {
+  res.status(200).send(' 👨‍🔬 All systems nominal. 🤦‍')
+})
+
 server.listen(port, () => {
   console.log(`------Listening on port ${port}!--------`)
 })
