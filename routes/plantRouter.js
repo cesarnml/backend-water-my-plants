a69edd69db54b67cfe650d9d2b5b1f607019edb6
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2019-03-11 12:23:01 -0700

    basic users and plants crud

--- routes/plantRouter.js
+++ routes/plantRouter.js
@@ -1,3 +1,8 @@
+const db = require('../data/dbConfig')
 const router = require('express').Router()
+const crudMethodMaker = require('../utils/crudMethodMaker')
+
+const methods = crudMethodMaker(db, 'plants')
+require('../utils/crudRouteMaker')(router, methods)
 
 module.exports = router
